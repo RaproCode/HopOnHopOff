03e6b6ef8af88e72d493707bae9038aec82217eb
Raja Prahara <raja.ardri@gmail.com>
2019-02-11 19:33:17 +0100

    Schema busTrip (for summary of user resa)

---
+++ models/bustrip-models.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const busSchema = new Schema(
+  {
+    // Document structure & rules defined here
+    userId: { type: Number, required: true },
+    lineId: { type: String, enum: ["Blue", "Red"], required: true },
+    capacity: { type: Number },
+    startDate: { type: Date, required: true },
+    hotel: { type: String },
+    image: { type: String },
+    visit: { type: String }
+  },
+  {
+    // Additional settings for the Scema class defined here
+    timestamps: true
+  }
+);
+
+// "Bus" model -> "buses" collection
+const Bus = mongoose.model("Bus", busSchema);
+
+// share the model with other parts of the app
+module.exports = Bus;
