b1336da21da4e85db4d9716bac4eee5ff4332a25
unknown <aurelie.benovici@gmail.com>
2019-02-11 09:57:06 +0100

    new repo

--- models/user-models.js
+++ models/user-models.js
@@ -2,24 +2,46 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  userName: { type: String, required: true },
-  userSurname: { type: String, required: true },
-  age: { type: Number, min: 18 },
-  adress: {
-    type: String //??
+const userSchema = new Schema(
+  {
+    // Document structure & rules defined here
+    // signUp name
+    lastName: { type: String, required: true, minlength: 2 },
+    firstName: { type: String, required: true, minlength: 2 },
+    // Mr or Ms/Mrs,Miss
+    age: { type: Number, min: 18 },
+    adress: [
+      {
+        streetName: { type: String, required: true, minlength: 2 } // Nedd to add match
+      },
+      {
+        streetNumber: { type: Number, required: true, min: 1, max: 4 }
+      },
+      {
+        zipCode: { type: String, minlength: 5 }
+      },
+      {
+        country: { type: String, enum: ["France", "England", "Spain"] } // Nedd to add more city
+      }
+    ],
+    email: { type: String, required: true, unique: true, match: /^.+@.+\..+$/ },
+    phoneNumber: { type: Number, unique: true, min: 10 },
+    encryptedPassword: { type: String, required: true, minlength: 6 },
+    passwordConfirmation: { type: String, required: true },
+    role: {
+      type: String,
+      required: true,
+      enum: ["normal", "admin"],
+      default: "normal"
+    }
   },
-  userContry: { type: String, match: /^[A-Z][A-Z]$/ },
-  email: { type: String, required: true },
-  phoneNumber: { type: String },
-  //ObjectId IS NOT a built-in Javascript class like String, Number, ect.
-  // MONGOOSE gives us the class throudh Schema.type
-  userId: [Schema.Types.ObjectId],
-  userPwd: { type: String, min: 6 }
-});
+  {
+    // Additional settings for the Scema class defined here
+    timestamps: true
+  }
+);
 
+// "User" model -> "users" collection
 const User = mongoose.model("User", userSchema);
 
-// Sharing the User variable with other files that requiere user-model.js
-//("User is Mongoose model that connects to the "User" collection)
 module.exports = User;
---
+++ routes/auth-router.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bcryptjs = require("bcryptjs");
+
+const User = require("../models/user-models.js");
+const router = express.Router();
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth-views/signup-form.hbs");
+});
+
+router.post("/process-signup", (req, res, next) => {
+  const {
+    lastName,
+    firstName,
+    email,
+    originalPassword,
+    passwordConfirmation
+  } = req.body;
+
+  if (!originalPassword || originalPassword.match(/[0-9]/)) {
+    req.flash("Error", "Password must countain min 6 characters and 1 number ");
+
+    res.redirect("/signup");
+
+    return;
+  }
+  // Password and Confirmation password
+  if (originalPassword != passwordConfirmation) {
+    req.flash(
+      "Error",
+      "The password and Confirmation password fiels must match",
+      "Please enter the same information in both field"
+    );
+
+    // Must be redirect or can we stay on the page with the flash message ??
+    req.redirect("/");
+  }
+
+  // Encrypt the user's password before saving
+  const encryptedPassword = bcryptjs.hashSync(originalPassword, 10);
+
+  User.create({ lastName, firstName, email, encryptedPassword })
+    .then(() => {
+      req.flash("Congratulation ");
+
+      // redirect to the HOME PAGE
+      res.redirect("/");
+    })
+    .catch(err => next(err));
+});
+
+module.exports = router;
