a919dc91d356e2dda14b58497707e66e221fc35d
Raja Prahara <raja.ardri@gmail.com>
2019-02-12 20:46:54 +0100

    send user to signup before continue

--- routes/resa-router.js
+++ routes/resa-router.js
@@ -5,6 +5,7 @@ const Busline = require("../models/bustrip-models");
 
 const router = express.Router();
 
+// result of user input -- Departure city
 router.get("/resa/:resaId", (req, res, next) => {
   City.find()
     .then(resaDoc => {
@@ -15,6 +16,21 @@ router.get("/resa/:resaId", (req, res, next) => {
     .catch(err => next(err));
 });
 
+// user need to sign up before continue to option itinerary
+router.get("/resa/:resaId/city/:cityName", (req, res, next) => {
+  // req.user comes from Passport's deserializeUser()
+  // (it's the document from the database of the logged-in user)
+  if (req.user) {
+    // AUTHORIZATION: only show the itinerary if you are logged-in
+    res.render("resa-views/resa-option.hbs");
+  } else {
+    // redirect to the sign up page if you ARE NOT logged-in
+    req.flash("error", "You have to be signUp to add a room. 🛌");
+    res.redirect("/signup");
+  }
+});
+
+// user choices of itinerary
 router.get("/resa/:resaId/city/:cityName", (req, res, next) => {
   const { resaId, cityName } = req.params;
 
@@ -27,5 +43,16 @@ router.get("/resa/:resaId/city/:cityName", (req, res, next) => {
     })
     .catch(err => next(err));
 });
+
+// Summary of user order
+// router.get("/resa/:resaId/:summary", (req, res, next) => {
+//   const { resaId, summary } = req.params;
+
+//   Busline.find({ "cities.startingCity": summary })
+//   .then( => {
+
+//   })
+//   .catch(err => next(err));
+// };
 
 module.exports = router;
